b3276a17f5e71737ecb8edb1aac5909c57f2e2ba
Elmer <elmer15222@gmail.com>
2020-03-07 01:48:50 -0500

    creado estructura html y css

---
+++ src/main.js
@@ -0,0 +1,10 @@
+function init() {
+  
+  let result = document.getElementById('result');
+  let C = document.getElementById('C');
+  let reset = document.getElementById('reset');
+  let porcentaje = document.getElementById('porcentaje');
+  let division = document.getElementById('division');
+  let multiplicacion = document.getElementById('multiplicacion');
+  let
+}
